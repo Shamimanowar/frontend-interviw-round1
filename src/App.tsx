03a050ec552da3b56e45bd3734d75bd8235e71b7
sohanemon <m.sohanemon@gmail.com>
2023-10-28 20:01:23 +0600

    * The font family is changed to 'Roboto', sans-serif. * The background color is changed to black. * The color is changed to white. * The grid is centered. * The grid template columns are changed to 6.66% repeat(3, 13.33% 4.44%) repeat(2, 13.33% 6.66%). * A new class 'block' is created. * The 'block' class has an aspect ratio of 3/1, a width of 100%, a background-color of white, a color of black, and a display of grid. * The 'block' class has a div with the text 'Problem'. * The 'block' class has a div with the text 'Requirements'. * The 'block' class has a div with the text 'New Product Idea'. * The 'block' class has a div with the text 'Research'.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 function App() {
   return (
     <main>
-      <div className='container grid'></div>
+      <div className='container grid'>
+        <div />
+        <div>
+          <div className='block'>Problem</div>
+        </div>
+        <div>hello</div>
+        <div>
+          {['Requirements', 'New Product Idea', 'Research'].map((el) => (
+            <div key={el} className='block'>
+              {el}
+            </div>
+          ))}
+        </div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+      </div>
     </main>
   );
 }
