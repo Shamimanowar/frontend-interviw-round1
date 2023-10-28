3fbcd407833b194d6a55d491bea42aafb0944072
sohanemon <m.sohanemon@gmail.com>
2023-10-28 21:51:00 +0600

    Update styles for arrows and chip components

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,13 @@ function App() {
         <div className='block magenta'>
           <span>Prototyping</span>
         </div>
-        <img className='arr-horizontal' src='/arr-right.svg' />
+        <div className='arr-container'>
+          <img className='arr-u arr-u1' src='/arr-u1.svg' />
+          <span className='chip chip-u1'>Needs Update</span>
+          <span className='chip chip-u2'>Rejected</span>
+          <img className='arr-u arr-u2' src='/arr-u2.svg' />
+          <img className='arr-horizontal' src='/arr-right.svg' />
+        </div>
         <div>
           <div className='block indigo'>Test</div>
           <img className='arr' src='/arr-up-down.svg' />
