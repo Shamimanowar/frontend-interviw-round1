d99a22beae39d7010f2a65f664242a2e03ecf629
sohanemon <m.sohanemon@gmail.com>
2023-10-28 21:22:45 +0600

    Update styles and assets for product workflow diagram

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,40 @@ function App() {
       <div className='container grid'>
         <div />
         <div>
-          <div className='block'>Problem</div>
+          <div className='block pink'>Problem</div>
         </div>
-        <div>hello</div>
+        <img className='arr-horizontal' src='/arr-right.svg' />
         <div>
-          {['Requirements', 'New Product Idea', 'Research'].map((el) => (
-            <div key={el} className='block'>
-              {el}
-            </div>
-          ))}
+          {['Requirements', 'arr', 'New Product Idea', 'arr', 'Research'].map(
+            (el) =>
+              el === 'arr' ? (
+                <img className='arr' src='/arr-up-down.svg' />
+              ) : (
+                <div key={el} className='block'>
+                  {el}
+                </div>
+              )
+          )}
         </div>
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
+
+        <img className='arr-horizontal' src='/arr-right.svg' />
+        <div className='block magenta'>
+          <span>Prototyping</span>
+        </div>
+        <img className='arr-horizontal' src='/arr-right.svg' />
+        <div>
+          <div className='block indigo'>Test</div>
+          <img className='arr' src='/arr-up-down.svg' />
+          <div className='block outline'>Evaluate</div>
+          <img
+            style={{ visibility: 'hidden' }}
+            className='arr'
+            src='/arr-up-down.svg'
+          />
+          <div style={{ visibility: 'hidden' }} className='block'></div>
+        </div>
+        <img className='arr-s' src='/arr-s.svg' />
+        <div className='block green'>Ship</div>
       </div>
     </main>
   );
